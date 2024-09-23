e89d1f87e36e8036e90103fe59291512b6c9075a
Lovebirdflying <gomitoguns@gmail.com>
2024-09-23 16:16:45 +0100

    First class

---
+++ basic.ts
@@ -0,0 +1,27 @@
+var Name:string = "First Class"
+console.log("This is My:", Name)
+
+var uid:number = 12;
+console.log("My ID:", uid)
+
+var Mary:string = "collegue";
+console.log("She is my", Mary);
+
+var isclassmate:boolean = true 
+console.log("She is my class mate :", isclassmate);
+
+var notFound:null = null;
+console.log("The data is", notFound);
+
+
+var noValue:undefined = undefined;
+
+console.log("The value is", noValue);
+
+
+var array = ['banana','bread','Mary','Ganiyu','DrPrince','Softzenit', true,false,
+    undefined, null,
+]
+console.log("These are the items in the array:", array[0], array[1], array[2], array[3], array[4], array[5], array[6],
+array[7],array[8], array[9]
+)
